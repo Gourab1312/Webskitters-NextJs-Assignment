15bedcbc7729a3081558aeefa08456c72b63a7ed
Gourab Bandyopadhyay <gourabbandyopadhyay.cgr@gmail.com>
2024-08-03 18:43:14 +0530

    updated the heading styles

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Typography } from "@mui/material";
 export default function Home() {
   return (
     <main className="py-4">
-      <Typography variant="h3" component="h1" align="center" gutterBottom>
+      <Typography className="text-2xl lg:text-5xl font-semibold w-full text-center pb-6 px-6 lg:px-12">
         Webskitters NextJs Coding Assignment
       </Typography>
       <ProductGrid />
